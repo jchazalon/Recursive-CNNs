5f6db271a375e5f395fb7e1158260c5f73af9949
Khurram Javed <14besekjaved@seecs.edu.pk>
2017-05-16 16:50:13 +0500

    Adding Small.py

--- Small.py
+++ Small.py
@@ -16,6 +16,16 @@ class get_corners_moreBG:
         mean_train = np.expand_dims(mean_train, axis=0)
         self.mean_train = np.expand_dims(mean_train, axis=0)
 
+        print self.mean_train
+        # [[[[ 146.55914183  139.54338589  154.33292928]]]]
+        # (array([  698.175354  ,  1161.4095459 ,  1302.14160156,   768.1395874 ], dtype=float32), array([ 181.6534729 ,  208.27745056,  821.59490967,  859.08441162], dtype=float32))
+        # [[[[ 146.55914183  139.54338589  154.33292928]]]]
+        # (1080, 1920, 3)
+        # (array([  697.51312256,  1160.88891602,  1302.05566406,   768.35913086], dtype=float32), array([ 181.32633972,  208.20285034,  821.5411377 ,  859.25219727], dtype=float32))
+        # (array([  700.22625732,  1163.03771973,  1303.61547852,   767.75402832], dtype=float32), array([ 183.30375671,  207.90759277,  821.47290039,  858.97912598], dtype=float32))
+        # (array([  700.51947021,  1131.64453125,  1302.01855469,   763.37438965], dtype=float32), array([ 192.44558716,  203.4969635 ,  835.41467285,  871.97808838], dtype=float32))
+        # (array([  700.51947021,  1131.64453125,  1302.01855469,   763.37438965], dtype=float32), array([ 192.44558716,  203.4969635 ,  835.41467285,  871.97808838], dtype=float32))
+        # (array([  700.22625732,  1163.03771973,  1303.61547852,   767.75402832], dtype=float32), array([ 183.30375671,  207.90759277,  821.47290039,  858.97912598], dtype=float32))
         def weight_variable(shape, name="temp"):
             initial = tf.truncated_normal(shape, stddev=0.1, name=name)
             return tf.Variable(initial)
@@ -36,14 +46,15 @@ class get_corners_moreBG:
 
 
         with tf.name_scope("Input"):
-            self.inputT = self.x = x = tf.placeholder(tf.float32, shape=[None, None, None, 3])
+            self.inputT = self.x = x = tf.placeholder(tf.float32, shape=[None, None, 3])
+            mean = tf.constant([[[146.55914183,  139.54338589,  154.33292928]]])
             self.x = x = tf.image.resize_images(self.x, [32, 32])
-            x_ = tf.image.random_brightness(x, 5)
-            x_ = tf.image.random_contrast(x_, lower=0.9, upper=1.1)
+            self.x = x = tf.subtract(self.x, mean)
+            self.x = x  =tf.reshape(self.x, [1,32,32,3])
         with tf.name_scope("Conv1"):
             W_conv1 = weight_variable([5, 5, 3, 20], name="W_conv1")
             b_conv1 = bias_variable([20], name="b_conv1")
-            h_conv1 = tf.nn.relu(conv2d(x_, W_conv1) + b_conv1)
+            h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
         with tf.name_scope("MaxPool1"):
             h_pool1 = max_pool_2x2(h_conv1)
         with tf.name_scope("Conv2"):
@@ -78,21 +89,15 @@ class get_corners_moreBG:
             h_conv5 = tf.nn.relu(conv2d(h_pool4, W_conv5) + b_conv5)
             h_pool5 = max_pool_2x2(h_conv5)
 
-
-        temp_size = h_pool5.get_shape()
-        temp_size = temp_size[1] * temp_size[2] * temp_size[3]
-        temp_size = int(temp_size)
-
         with tf.name_scope("FCLayers"):
-            W_fc1 = weight_variable([int(temp_size), 500], name="W_fc1")
+            W_fc1 = weight_variable([100, 500], name="W_fc1")
             b_fc1 = bias_variable([500], name="b_fc1")
 
-            h_pool4_flat = tf.reshape(h_pool5, [-1, temp_size])
+            h_pool4_flat = tf.reshape(h_pool5, [-1, 100])
             h_fc1 = tf.nn.relu(tf.matmul(h_pool4_flat, W_fc1) + b_fc1)
 
 
-            self.keep_prob = keep_prob = tf.placeholder(tf.float32)
-            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+            h_fc1_drop = h_fc1
 
 
             W_fc2 = weight_variable([500, 500], name="W_fc2")
@@ -105,6 +110,8 @@ class get_corners_moreBG:
             b_fc3 = bias_variable([8], name="b_fc3")
 
             self.y_conv =y_conv = tf.matmul(y_conv, W_fc3) + b_fc3
+            div_cont = tf.constant([32.0])
+            self.scaledoutput = tf.divide(self.y_conv, div_cont)
 
 
 
@@ -116,15 +123,15 @@ class get_corners_moreBG:
         else:
             init = tf.global_variables_initializer()
             self.sess.run(init)
-    def get(self,img):
-        img_temp = np.expand_dims(img, axis=0)
-        img_temp = img_temp - self.mean_train
-        # print self.mean_train
-        # [[[[ 146.55914183  139.54338589  154.33292928]]]]
+        tf.train.write_graph(self.sess.graph_def, "../", "mymodel.pbtxt")
+    def get(self,img_temp):
+        # img_temp = img_temp - self.mean_train[0]
 
-        response = self.y_conv.eval(feed_dict={
-            self.inputT: img_temp, self.keep_prob: 1.0}, session=self.sess)
-        response = response[0]/32
+        response = self.scaledoutput.eval(feed_dict={
+            self.inputT: img_temp}, session=self.sess)
+        # response = response[0]/32
+        print "Reponse", response
+        response = response[0]
         x = response[[0,2,4,6]]
         y = response[[1,3,5,7]]
         x = x*img.shape[1]
